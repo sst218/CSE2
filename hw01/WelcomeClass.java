371032db3599b9a94f3f727024f73d2257273176
sst218 <sst218@lehigh.edu>
2014-09-02 19:30:24 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//Stutti Tilwa
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+    
+//  print the statment with the box around it
+    System.out.println("   -----------");
+    System.out.println("   | WELCOME |");
+    System.out.println("   -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println(" |-S--S--T--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v  v");
+    System.out.println("My name is Stutti Tilwa and I live in Montville, New Jersey.");
+    System.out.println("But I went to school at Newark Academy in Livingston, New Jersey");
+    System.out.println("I have brown hair and I am barely five feet tall");
+
+    //test/testtest
+    }
+}
