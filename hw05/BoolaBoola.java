d5c02c5cf570caf63425814a5d4a8d969797f859
sst218 <sst218@lehigh.edu>
2014-09-30 18:23:22 +0000

    updated BurgerKing

--- hw05/BoolaBoola.java
+++ hw05/BoolaBoola.java
@@ -16,14 +16,125 @@ public class BoolaBoola{
 
 Scanner myScanner;
     myScanner = new Scanner (System.in); //Scanner constructor
+//declaring variables 
+int Boola1, Boola2, Boola3;
+//generating randoms
+int operatorBoola1= (int)(Math.random()*2);
+int operatorBoola2= (int)(Math.random()*2);
+//generating randoms for booleans
+Boola1=(int)(Math.random()*2);
+Boola2=(int)(Math.random()*2);
+Boola3=(int)(Math.random()*2);
+// using switch statements to assign operators 
+String operator1= "";
+    switch (operatorBoola1) //random operator generating 
+    {
+        case 0: 
+            operator1 = "&&" ;
+        break;
+        case 1:
+            operator1 = "||" ;
+        break;
+    }
 
+String operator2 = "";
+    switch (operatorBoola2) //random operator generating 
+    {
+        case 0:
+            operator2 = "&&" ; 
+        break;
+        case 1:
+            operator2 = "||" ;
+        break;
+    }
+//using switch statements to assign true or false values for first Boolean
+String boolaString1 = "";
+    switch (Boola1) 
+   {
+        case 0:
+            boolaString1 = "true";
+        break;
+        case 1:
+            boolaString1 = "false";
+        break;
+    }
+//using switch statements to assign true or false values for second Boolean
+String boolaString2 = "";
+    switch (Boola2)
+    {
+        case 0:
+            boolaString2 = "true";
+        break;
+        case 1:
+            boolaString2 = "false";
+        break;
+    }
+//using switch statements to assign true or false values for third Boolean
+String boolaString3 = "";
+    switch (Boola3)
+    {
+        case 0:
+            boolaString3 = "true";
+        break;
+        case 1:
+            boolaString3 = "false";
+        break;
+    }
 //program should then present the expression to the user and ask the user to state the result of of the expression
-System.out.println("Enter a letter to indicate a choice of ");
+System.out.print("Does " + boolaString1 + " " + operator1+ " " + boolaString2+ " " + operator2+ " " + boolaString3 + " " + "have the value true(t/T) or false(f/F)? ");
+String userInput = myScanner.next();
 
-String answer=myScanner.next();      
-      
-      
-      
+String realAnswer = "";
+//below is part of program that stores the answers to statements 
+/* if(userInput.equals("T") || userInput.equals("t") || userInput.equals("F") || userInput.equals("f")) {
+    
+if ((operator1 = "&&") && (operator2 = "&&") ) {
+    if(boolaString1 = true && boolaString2 = true && boolaString3 = true) { 
+    realAnswer = true;    
         
     }
+    else 
+    realAnswer = false;
+    }
+    
+if ((operator1 = "||") && (operator2 = "||")) {
+    if((boolaString1 = true) || (boolaString2 = true) || (boolaString3 = true) ) {
+    realAnswer = true;
+    }
+    else
+    realAnswer = false;
+    }
+
+if ((operator1 = "&&") && (operator2 = "||")) {
+    if ((boolaString1 = true) && (boolaString2 = true) || (boolaString3=true)){
+    realAnswer = true;
+    }
+    else
+    realAnswer = false;
+    } 
+
+if ((operator1 = "||") && (operator2 = "&&")) {
+    if ((boolaString1 =true) || (boolaString2 = true) && (boolaString3 = true)) {
+    realAnswer = true;  
+    }
+    else 
+    realAnswer = false;
+    }
+
+if (realAnswer == false) {
+    System.out.println("Wrong, try again"); //if user entered wrong answer
 }
+else if (realAnswer == true) {
+    System.out.println("Correct!"); //if user entered right answer
+}
+else {
+    System.out.println("Wrong, try again!"); 
+}
+} 
+    else{
+        System.out.println("You did not enter one of 't', 'T', 'F', 'f'");
+        return; */
+        
+                
+    } //end of main method
+} //end of class
--- hw05/BurgerKing.java
+++ hw05/BurgerKing.java
@@ -23,7 +23,8 @@ System.out.println("Soda (S or s)");
 System.out.print("Fries (F or f) ");
 //accepts user input
 String input1=myScanner.next();
-
+//using switch for when user gets a burger, soda, fries
+//asks further questions about burger choice, soda choice or fries choice
 switch (input1)
 {
     case("B"):
@@ -31,30 +32,64 @@ switch (input1)
     System.out.println("Enter A or a for 'all the fixins'");
     System.out.println("C or c for cheese)");
     System.out.print("N or n for none of the above ");
-    break;
-        String input2=myScanner.next();
+        String input2=myScanner.next(); //further questions about burgers
         switch (input2) 
         {
         case("C"):
         case("c"):
-        System.out.println("You ordered a burger with cheese")
+        System.out.println("You ordered a burger with cheese");
         break;
         case("N"):
         case("n"):
-        System.out.println("You ordered just a burger with none of the above")
+        System.out.println("You ordered just a burger");
         break;
         case("A"):
         case("a"):
-        System.out.println("You ordered a burger with 'all the fixins'")
+        System.out.println("You ordered a burger with 'all the fixins'");
         break;
         }
+        break;
     case("S"):
     case("s"):
-    break;
-        
+    System.out.println("Do you want Pepsi (p or P),");
+    System.out.print("Coke (c or C), Sprite (s or S) or Mountain Dew (M or m)- ");
+        String input3=myScanner.next(); //further questions about soda
+        switch (input3) 
+        {
+        case("P"):
+        case("p"):
+        System.out.println("You ordered a Pepsi");
+        break;
+        case("C"):
+        case("c"):
+        System.out.println("You ordered a Coke");
+        break;
+        case("M"):
+        case("m"):
+        System.out.println("You ordered a Mountain Dew");
+        break;
+        }
+        break;
     case("F"):
     case("f"):
-    break;    
+    System.out.print("Do you want a large or small order of fries (l,L,s, or S");
+        String input4=myScanner.next(); //further questions about fries
+        switch (input4)
+        {
+        case("L"):
+        case("l"):
+        System.out.println("You ordered large fries");
+        break; 
+        case("S"):
+        case("s"):
+        System.out.println("You ordered small fries");
+        break;
+        }
+        break;   
+        default:
+        System.out.println("You did not enter any of B, b, S, s, F, or f"); //makes sure user enters character needed
+        
+        
 } //end of first swtich 
         
         
